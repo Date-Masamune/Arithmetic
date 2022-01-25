f6bfa5607b614e77c7ca94cd673a8b09b8aabb53
jacob gishwiller <sirbruce13@yahoo.com>
2022-01-25 14:51:28 -0700

    test

--- Arithmetic/Program.cs
+++ Arithmetic/Program.cs
@@ -3,10 +3,26 @@
 namespace Arithmetic
 {
    class Program
+   
+   class Addittion
+   class Subtraction
+   class Multiplication
+   class Division
    {
       static void Main(string[] args)
       {
-         Console.WriteLine("Hello World!");
+         int number1; //declare first number to add
+         int number2; //declare second number to add
+         int sum // declare sume of number1 and number2
+
+
+         Console.Write("Enter first integer "); // prompt user
+         // read first number from user
+         number1 = int.Parse(Console.ReadLine());
+
+         Console.Write("Enter second integer"); //prompt user
+         //read second number from user
+         number2 = int.Parse(Console.ReadLine());
       }
    }
 }
